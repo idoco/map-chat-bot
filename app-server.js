e9b75ef8852a26116869fd7adbbb8711c451bbf7
Ido Cohen <ido.c.mail@gmail.com>
2015-12-26 18:27:24 +0200

    Small refactor in app-server.js

--- app-server.js
+++ app-server.js
@@ -7,87 +7,81 @@ var ElizaBot = require('./elizabot');
 // to - new WebsocketDriver(this.url, [], {headers: {Origin: 'http://idoco.github.io/map-chat/'}});
 // inorder to add the origin header to the websocket registration. Otherwise it will be rejected by the map-chat server
 
-var eliza = new ElizaBot(),
-    eb,
-    sessionId,
-    retryCount = 10,
-    rateLimitTimer = 0;
-
 
 var possibleLocations = [
     {lat: 64.1417151, lng: -21.9318432},
     {lat: 41.7269933, lng: 44.7627975},
     {lat: 32.6575252, lng: -16.912832}
 ];
-var location = possibleLocations[Math.floor(Math.random()*possibleLocations.length)]
-
+var location = possibleLocations[Math.floor(Math.random()*possibleLocations.length)];
+var eliza = new ElizaBot();
 init();
 
+function init() {
+    var eventBus = new vertx.EventBus("http://chatmap.cloudapp.net/chat");
+    eventBus.onopen = onopen;
+    eventBus.onclose = onclose;
+    eventBus.retryCount = 10;
+}
+
 function onopen () {
-    this.registerHandler("main", function (msg) {
+    var eventBus = this,
+        sessionId,
+        rateLimitTimer = 0;
+
+    eventBus.registerHandler("main", function (msg) {
         if (sessionId) {
-            processMessage(msg);
+            handleMessage(msg);
         } else {
             sessionId = msg.newSessionId;
             console.log("First message received. sessionId is [" + sessionId + "]");
             publish("Hello world! :)");
         }
     });
+
+    function publish(text) {
+        var currentTime = new Date().getTime();
+        if (currentTime - rateLimitTimer < 1000) {
+            console.log("Rate too high, dropping message");
+            return;
+        }
+        eventBus.publish("main", {
+            lat: location.lat, lng: location.lng, text: text
+        });
+        rateLimitTimer = currentTime;
+    }
+
+    function handleMessage(msg) {
+        var myMessage = msg.sessionId === sessionId;
+
+        if (!myMessage) { // not my message
+            var country = crg.get_country(msg.lat, msg.lng);
+            var countryName = country && country.name ? country.name : 'unknown';
+            if (msg.text) {
+                console.log(countryName + " : " + msg.text);
+            }
+
+            var userFirstMessage = !msg.text;
+            if (userFirstMessage) {
+                publish("Hi there "+countryName + "!")
+            } else {
+                var answer = eliza.transform(msg.text);
+                setTimeout(function(){
+                    publish(answer);
+                }, 3000);
+            }
+        } else {
+            console.log("myBot : " + msg.text);
+        }
+    }
 }
 
 function onclose () {
-    if (retryCount) {
-        retryCount--;
+    if (this.retryCount) {
+        this.retryCount--;
         console.log('Connection lost, scheduling reconnect');
         setTimeout(init, 1000);
     } else{
         console.log('Connection lost, please restart :( ');
     }
 }
-
-function init () {
-    eb = new vertx.EventBus("http://chatmap.cloudapp.net/chat");
-    eb.onopen = onopen;
-    eb.onclose = onclose;
-}
-
-function publish(text) {
-    var currentTime = new Date().getTime();
-    if (currentTime - rateLimitTimer < 1000) {
-        console.log("Rate too high, dropping message");
-        return;
-    }
-    eb.publish("main", {
-        lat: location.lat, lng: location.lng, text: text
-    });
-    rateLimitTimer = currentTime;
-}
-
-function processMessage(msg) {
-    var myMessage = msg.sessionId === sessionId;
-
-    if (!myMessage) { // not my message
-        var country = crg.get_country(msg.lat, msg.lng);
-        var countryName = country && country.name ? country.name : 'unknown';
-        if (msg.text) {
-            console.log(countryName + " : " + msg.text);
-        }
-
-        var userFirstMessage = !msg.text;
-        if (userFirstMessage) {
-            publish("Hi there "+countryName + "!")
-        } else {
-            var answer = eliza.transform(msg.text);
-            setTimeout(function(){
-                publish(answer);
-            }, 3000);
-        }
-    } else {
-        console.log("myBot : " + msg.text);
-    }
-}
-
-
-
-
-
